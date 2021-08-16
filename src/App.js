5e012111cfd4b2028c091673090cd91e7661557f
Georgeleeh <me+code@georgeleeh.com>
2021-08-16 12:28:55 +0100

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { CssBaseline, makeStyles, Typography } from "@material-ui/core";
+import React from "react";
+import { CssBaseline, makeStyles } from "@material-ui/core";
 import NavBar from "./components/NavBar";
-import { BlogFeed } from "./components/BlogFeed";
+import Home from "./views/home";
+import { Route, Switch } from "react-router-dom";
+import Blog from "./views/blog";
 
 const useStyles = makeStyles({
   root: {
@@ -22,67 +24,21 @@ const useStyles = makeStyles({
 });
 
 function App() {
-  const [blogposts, setBlogposts] = useState([]);
   const classes = useStyles();
-
-  useEffect(() => {
-    fetch("http://localhost:5000/blogpost").then((response) =>
-      response.json().then((data) => {
-        console.log(data.blogposts);
-        setBlogposts(data.blogposts);
-      })
-    );
-  }, []);
-
-  function featuredPost(blogpost) {
-    return blogpost.featured;
-  }
-
-  function notFeaturedPost(blogpost) {
-    return !blogpost.featured;
-  }
 
   return (
     <div className="App">
       <CssBaseline />
       <NavBar />
       <div className={classes.root}>
-        <div className={classes.main_body}>
-          <Typography
-            className={classes.main_title}
-            variant="h3"
-            component="h1"
-          >
-            George Makes Stuff
-          </Typography>
-          <Typography
-            className={classes.text_body}
-            variant="body1"
-            component="p"
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Typography>
-        </div>
-        <div className={classes.feeds}>
-          <BlogFeed
-            blogposts={blogposts.filter(featuredPost)}
-            feed_name={"⭐ Featured Posts"}
-          />
-          <BlogFeed
-            blogposts={blogposts.slice(-3, blogposts.length).reverse()}
-            feed_name={"🔥 New Posts"}
-          />
-          <BlogFeed
-            blogposts={blogposts.filter(notFeaturedPost)}
-            feed_name={"🍔 Not Featured Posts"}
-          />
-        </div>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/blog" exact>
+            <Blog />
+          </Route>
+        </Switch>
       </div>
     </div>
   );
