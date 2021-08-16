beb307cd2daeb9eeadb9af85dfe6e918a8fcac86
Georgeleeh <me+code@georgeleeh.com>
2021-08-16 12:29:24 +0100

    implement routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,54 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Button,
+  ButtonBase,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(6),
   },
   title: {
     flexGrow: 1,
+    color: "#FFFFFF",
+  },
+  button: {
+    color: "#FFFFFF",
   },
 }));
 
 function NavBar() {
   const classes = useStyles();
+  const history = useHistory();
+
+  function handleClick(path) {
+    history.push(path);
+  }
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" className={classes.title}>
-          Georgeleeh Blog
-        </Typography>
+        <ButtonBase onClick={() => handleClick("")}>
+          <Typography variant="h6" className={classes.title}>
+            George Harris
+          </Typography>
+        </ButtonBase>
+        <div className={classes.menuButton}>
+          <Button
+            className={classes.button}
+            onClick={() => handleClick("blog")}
+          >
+            Blog
+          </Button>
+        </div>
       </Toolbar>
     </AppBar>
   );
